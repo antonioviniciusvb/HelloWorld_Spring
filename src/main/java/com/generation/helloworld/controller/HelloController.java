7494defb22271b1ff0a4899bcc6477f630cda79e
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2023-12-07 12:37:08 -0300

    Implementation of GET methods for the canvas

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -13,9 +13,18 @@ public class HelloController {
 		return "<b>Hello turma 68!!!</b>";
 	}
 	
-	@GetMapping("/top")
-	public String top() {
-		return "<i>Hello turma 68 TOP!!!<i>";
+	@GetMapping("/bsmGeneration")
+	public String bsmGeneration() {
+		return "<b>BSM’s da Generation Brasil</b><br><br>"
+				+ "<b>[Mentalidades]</b><br>"
+				+ "Persistência<br>Responsabilidade pessoal<br>Orientação ao futuro<br>Mentalidade de crescimento<br><br>"
+				+ "<b>[Comportamentais]</b><br>"
+				+ "Comunicação<br>Proatividade<br>Orientação aos detalhes<br>Trabalho em Equipe";
+	}
+	
+	@GetMapping("/goals")
+	public String goals() {
+		return "Learn about spring and start implementing the \"Personal Blog\" project.";
 	}
 
 }
